2f26b66cc83a82924001b9476f7db968d023f9c6
Rafael <rafaeloliveiracds@gmail.com>
2023-04-06 15:32:19 -0300

    Feat // Styled components, global style, theme

---
+++ Frontend/styled.d.ts
@@ -0,0 +1,8 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+	export interface DefaultTheme {
+		mainBackground: string;
+		mainText: string;
+	}
+}
---
+++ Frontend/styles/theme.tsx
@@ -0,0 +1,8 @@
+import { DefaultTheme } from 'styled-components';
+
+export const theme: { [key: string]: DefaultTheme } = {
+	dark: {
+		mainBackground: '#101010',
+		mainText: '#ddd',
+	},
+};
