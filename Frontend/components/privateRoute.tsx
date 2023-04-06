829ab7ea27d1826d913d6773eecdd7b688d96ef6
Rafael <rafaeloliveiracds@gmail.com>
2023-04-06 14:42:50 -0300

    Feat // High order component, private route

--- Frontend/components/privateRoute.tsx
+++ Frontend/components/privateRoute.tsx
@@ -1,20 +1,41 @@
 import Authentication from '@/pages/authentication';
+import { storageKeys, StorageUser } from '@/interfaces/interfaceStorageKeys';
+import { IValidateToken, ResponseValidateToken } from '@/interfaces/queriesUser';
+import { client } from '@/services/client';
+import { TypesQueriesUser } from '@/services/queries/user';
 import { NextComponentType } from 'next';
 import { useEffect, useState } from 'react';
 
 const privateRoute = <T extends JSX.IntrinsicAttributes>(Component: NextComponentType<T>) => {
 	const Auth = (props: T) => {
+		const typesQueriesUser = new TypesQueriesUser();
 		const [isLoading, setIsLoading] = useState(true);
-		const isLoggedIn = true;
+		const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+		const verifyAuthentication = async () => {
+			const storage = localStorage.getItem(storageKeys.user);
+			if (!storage) return setIsLoading(false);
+
+			const { token } = JSON.parse(storage) as StorageUser;
+			if (!token) return setIsLoading(false);
+
+			const { data } = await client.query<ResponseValidateToken, IValidateToken>({
+				query: typesQueriesUser.VALIDATE_TOKEN,
+				variables: { token },
+			});
+
+			if (data.validateToken.message === 'false') return setIsLoading(false);
+
+			setIsLoading(false);
+			setIsLoggedIn(true);
+		};
 
 		useEffect(() => {
-			setTimeout(() => {
-				setIsLoading(false);
-			}, 2000);
+			verifyAuthentication();
 		}, []);
 
+		// todo > Loading component
 		if (isLoading) return <h1>Loading...</h1>;
-
 		if (!isLoggedIn) return <Authentication />;
 
 		return <Component {...props} />;
---
+++ Frontend/interfaces/interfaceStorageKeys.ts
@@ -0,0 +1,8 @@
+export interface StorageUser {
+	email: string;
+	token: string;
+}
+
+export const storageKeys = {
+	user: 'user',
+};
