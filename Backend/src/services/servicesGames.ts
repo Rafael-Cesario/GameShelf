486e3e07d3f899799b2181b455f2f92edc9cc035
Rafael <rafaeloliveiracds@gmail.com>
2023-04-17 00:39:03 -0300

    Feat // Service get games

--- Backend/src/services/servicesGames.ts
+++ Backend/src/services/servicesGames.ts
@@ -1,7 +1,17 @@
+import { GraphQLError } from 'graphql';
 import { IAddGame, IRemoveGame, IGetGames, IUpdateGame } from '../interfaces/IGames';
+import { Errors } from '../interfaces/IResponses';
+import { ModelGames } from '../models/modelGames';
 
 export class ServicesGames {
-	async getGames({ email }: IGetGames) {}
+	async getGames({ email }: IGetGames) {
+		if (!email) throw new GraphQLError(Errors.emptyVariable + 'Email was not provided');
+
+		const user = await ModelGames.findOne({ user: email });
+		if (!user) throw new GraphQLError(Errors.userNotFound);
+
+		return { games: user.games };
+	}
 
 	async addGame({ addGame }: IAddGame) {}
 
