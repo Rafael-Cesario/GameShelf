7bd8fc14850dd4865ebbdc7d95cf758b265c946f
Rafael <rafaeloliveiracds@gmail.com>
2023-05-01 21:26:03 -0300

    Feat // Pick a random game from list

--- Frontend/features/index/components/header/randomGame.tsx
+++ Frontend/features/index/components/header/randomGame.tsx
@@ -1,16 +1,17 @@
+import { useDispatch } from 'react-redux';
 import { useFilteredGames } from '../../hooks/useFilteredGames';
+import { sliceGames } from '../../slices/games';
 
 export const RandomGame = () => {
 	const { games } = useFilteredGames();
 
+	const dispatch = useDispatch();
+
 	const pickRandomGame = () => {
 		const gamesLength = games.length - 1;
 		const randomIndex = Math.round(Math.random() * gamesLength);
+		dispatch(sliceGames.actions.setGameDetails({ gameDetails: { isOpen: 'details', gameName: games[randomIndex].name } }));
 	};
 
-	return (
-		<>
-			<button onClick={() => pickRandomGame()}>Aleatório</button>
-		</>
-	);
+	return <button onClick={() => pickRandomGame()}>Aleatório</button>;
 };
