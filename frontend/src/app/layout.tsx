74e19db74213307f1df7039ef5e4b8f0d8adfd30
Rafael <rafaeloliveiracds@gmail.com>
2023-11-01 06:29:02 -0300

    [ Feat ] Apollo client provider

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,6 @@
-import StyledComponentsRegistry from "@/lib/registry";
-import { StyledGlobal } from "@/styles/styled-global";
 import type { Metadata } from "next";
+import { Providers } from "@/lib/providers";
+import { StyledGlobal } from "@/styles/styled-global";
 import { Roboto_Slab } from "next/font/google";
 
 const roboto_slab = Roboto_Slab({ subsets: ["latin"] });
@@ -14,10 +14,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 	return (
 		<html lang="pt-br">
 			<body className={roboto_slab.className}>
-				<StyledComponentsRegistry>
+				<Providers>
 					<StyledGlobal />
 					{children}
-				</StyledComponentsRegistry>
+				</Providers>
 			</body>
 		</html>
 	);
