3e231c3c196f04ac5afa1fb0d025df80efb31bbd
Rafael <rafaeloliveiracds@gmail.com>
2023-11-15 18:57:11 -0300

    [ Feat ] Apollo client

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,6 @@
-import StyledComponentsRegistry from "@/lib/styled-components";
-import { GlobalStyled } from "@/styles/global-styled";
 import type { Metadata } from "next";
+import { Providers } from "@/lib/providers";
+import { GlobalStyled } from "@/styles/global-styled";
 import { Roboto_Slab } from "next/font/google";
 
 const roboto_slab = Roboto_Slab({ subsets: ["latin"] });
@@ -11,17 +11,17 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children, authentication }: { children: React.ReactNode; authentication: React.ReactNode }) {
-	// Todo > 
+	// Todo >
 	const isLoggedIn = false;
 
 	return (
 		<html lang="pt-br">
 			<body className={roboto_slab.className}>
-				<StyledComponentsRegistry>
+				<Providers>
 					<GlobalStyled />
 					{isLoggedIn || authentication}
 					{isLoggedIn && children}
-				</StyledComponentsRegistry>
+				</Providers>
 			</body>
 		</html>
 	);
